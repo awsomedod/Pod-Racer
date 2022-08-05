1dba28049937fe4bed4566f453a696fe6d1a9fb5
Ryan Soohoo <ryansoohoo@berkeley.edu>
2022-08-05 11:27:06 -0700

    changes toenemy AI and path

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
+	Rigidbody rb;
 	public Map map;
 	public RaceManager race;
 	public int currentNode;
@@ -17,6 +18,7 @@ public class Enemy : MonoBehaviour
 	void Start()
 	{
 		map = FindObjectOfType<Map>();
+		rb.GetComponent<Rigidbody>();
 	}
 
 	public void StartRace()
@@ -25,30 +27,59 @@ public class Enemy : MonoBehaviour
 		hasRaceStarted = true;
 	}
 	public NavMeshAgent agent;
-	public float nodeDistance;
-	public float randomPos;
+	public float nodeDetectionRadius;
+	public float randomNodeOffset;
 	void Update()
 	{
+		//DistanceLeftOnTrack();
 		if (!hasRaceStarted)
 			return;
 
 		float distanceToNextNode = Vector3.Distance(transform.position, map.map[currentNode].transform.position);
-		if (distanceToNextNode <= nodeDistance)
+		if (distanceToNextNode <= nodeDetectionRadius)
 		{
 			currentNode++;
-			Vector3 offset = Random.insideUnitSphere * randomPos;
+			Vector3 offset = Random.insideUnitSphere * randomNodeOffset;
 			offset.y = 0;
 			agent.SetDestination(map.map[currentNode].transform.position + offset);
 		}
 	}
 
-	public void HitMe()
+	public void ShootMe()
     {
 		if (hitProcessCoroutine != null)
 			StopCoroutine(hitProcessCoroutine);
 		hitProcessCoroutine = StartCoroutine(HitProcess());
 
 	}
+
+	public void HitMe()
+    {
+		if (ragdollProcessCoroutine != null)
+			StopCoroutine(ragdollProcessCoroutine);
+		hitProcessCoroutine = StartCoroutine(RagdollProcess());
+	}
+
+	Coroutine ragdollProcessCoroutine;
+	public float hitForce;
+	public IEnumerator RagdollProcess()
+	{
+		agent.enabled = false;
+		rb.AddExplosionForce(hitForce, transform.position, 5);
+		yield return new WaitForSeconds(3f);
+		agent.enabled = true;
+		agent.SetDestination(map.map[currentNode].transform.position);
+	}
+
+	public void DistanceLeftOnTrack()
+    {
+		float returnVal = 0;
+		for (int i = currentNode; i < map.map.Count; i++)
+        {
+			Debug.DrawLine(map.map[i].transform.position,map.map[i + 1].transform.position);
+		}
+    }
+
 	Coroutine hitProcessCoroutine;
 	public IEnumerator HitProcess()
 	{
